bffbc0dca0094ab4d9e7f731fb91f061bfad5bc2
Michael Shokoya <michaelshokoya@Michaels-MacBook-Pro.local>
2020-03-17 21:58:56 +0000

    login successful

--- client/src/sections/LogIn/index.tsx
+++ client/src/sections/LogIn/index.tsx
@@ -1,77 +1,83 @@
-import React, {useEffect, useRef} from "react";
-import { Card, Layout, Typography, Spin } from "antd";
-import { Viewer } from "../../lib/types";
-import {useApolloClient, useMutation} from '@apollo/react-hooks';
-import { AUTH_URL } from "../../lib/graphql/queries";
-import { AuthUrl as AuthUrlData } from "../../lib/graphql/queries/AuthUrl/__generated__/AuthUrl";
-import {LOG_IN} from '../../lib/graphql/mutations';
-import {LogIn as LogInData, LogInVariables} from '../../lib/graphql/mutations/LogIn/__generated__/LogIn';
+import React, { useEffect, useRef } from "react";
+import { Redirect } from "react-router-dom";
+import { useApolloClient, useMutation } from "@apollo/react-hooks";
+import { Card, Layout, Spin, Typography } from "antd";
 import { ErrorBanner } from "../../lib/components";
+import { LOG_IN } from "../../lib/graphql/mutations";
+import { AUTH_URL } from "../../lib/graphql/queries";
+import {
+  LogIn as LogInData,
+  LogInVariables
+} from "../../lib/graphql/mutations/LogIn/__generated__/LogIn";
+import { AuthUrl as AuthUrlData } from "../../lib/graphql/queries/AuthUrl/__generated__/AuthUrl";
 import { displaySuccessNotification, displayErrorMessage } from "../../lib/utils";
+import { Viewer } from "../../lib/types";
 
 // Image Assets
 import googleLogo from "./assets/google_logo.jpg";
-
-const { Content } = Layout;
-const { Text, Title } = Typography;
 
 interface Props {
   setViewer: (viewer: Viewer) => void;
 }
 
+const { Content } = Layout;
+const { Text, Title } = Typography;
+
 export const Login = ({ setViewer }: Props) => {
   const client = useApolloClient();
   const [
     logIn,
-    {data: logInData, loading: logInLoading, error: logInError}
+    { data: logInData, loading: logInLoading, error: logInError }
   ] = useMutation<LogInData, LogInVariables>(LOG_IN, {
     onCompleted: data => {
       if (data && data.logIn) {
         setViewer(data.logIn);
-        displaySuccessNotification('You\'ve successfully logged in!')
+        displaySuccessNotification("You've successfully logged in!");
       }
     }
   });
-  const logInRef= useRef(logIn);
-  
-  useEffect(() => {
-    const code = new URL(window.location.href).searchParams.get('code');
+  const logInRef = useRef(logIn);
 
-    if (code){
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get("code");
+    if (code) {
       logInRef.current({
         variables: {
-          input: {code}
+          input: { code }
         }
       });
     }
-
   }, []);
 
-  const handleAuthorize = async() => {
+  const handleAuthorize = async () => {
     try {
-      const {data} = await client.query<AuthUrlData>({
+      const { data } = await client.query<AuthUrlData>({
         query: AUTH_URL
-      })
-      window.location.href = data.authUrl
-    } catch (error) {
+      });
+      window.location.href = data.authUrl;
+    } catch {
       displayErrorMessage(
         "Sorry! We weren't able to log you in. Please try again later!"
-      )
+      );
     }
-  }
-
-  const logInErrorBannerElement = logInError ? (
-    <ErrorBanner description="We weren't able to log you in. Please try again soon."/>
-  ) : null;
+  };
 
   if (logInLoading) {
     return (
-      <Content className='log-in'>
-        <Spin size='large' tip='logging you in...' />
+      <Content className="log-in">
+        <Spin size="large" tip="Logging you in..." />
       </Content>
-    )
+    );
   }
 
+  if (logInData && logInData.logIn) {
+    const { id: viewerId } = logInData.logIn;
+    return <Redirect to={`/user/${viewerId}`} />;
+  }
+
+  const logInErrorBannerElement = logInError ? (
+    <ErrorBanner description="Sorry! We weren't able to log you in. Please try again later!" />
+  ) : null;
 
   return (
     <Content className="log-in">
@@ -88,7 +94,7 @@ export const Login = ({ setViewer }: Props) => {
           </Title>
           <Text>Sign in with Google to start booking available rentals!</Text>
         </div>
-        <button className="log-in-card__google-button" onClick={handleAuthorize} >
+        <button className="log-in-card__google-button" onClick={handleAuthorize}>
           <img
             src={googleLogo}
             alt="Google Logo"
